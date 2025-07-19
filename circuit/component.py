5708972f680e8557d370710ab6f8f883aa2e3311
AndreaBarletta <10665046@polimi.it>
2025-07-19 16:50:05 +0200

    Refactor all the interfrometer class and generalized components (still has to implement the barrier)

---
+++ circuit/component.py
@@ -0,0 +1,77 @@
+import numpy as np
+from enum import Enum
+
+class component_type(Enum):
+    """Enum to define the type of component in the interferometer"""
+    BEAM_SPLITTER = 1
+    PHASE_SHIFTER = 2
+    LOSS_ELEMENT = 3
+    BARRIER = 4
+    
+class component_color(Enum):
+    """Enum to define the color of the component in the interferometer"""
+    BEAM_SPLITTER = "lime"
+    PHASE_SHIFTER = "blueviolet"
+    LOSS_ELEMENT = "red"
+    BARRIER = "" # Empty since it's not drawn
+    LINE = "black"
+    OUTPUT_PHASE = "deeppink"
+    
+
+class component:
+    """This class defines a component in the interferometer.
+    It is used to represent a beam splitter, a phase shifter or a loss element.
+
+    Beam splitter:
+        - Transformation is:
+
+            e^{iphi}*cos(theta)     -sin(theta)
+            e^{iphi}*sin(theta)     cos(theta)
+    
+        - Modes are specified by modes, which is a tuple of two integers (mode1, mode2).
+        - Parameters are specified by params, which is a tuple (theta, phi).
+
+
+    - Phase shifter is:
+        - Transformation is:
+        
+            e^{iphi}
+        
+        - Modes are specified by modes, which is a tuple of one integer (mode).
+        - Parameters are specified by params, which is a float (phi).
+        
+        
+    - Loss element:
+        - Transformation is:
+        
+            sqrt(loss)
+            
+        - Modes are specified by modes, which is a tuple of one integer (mode).
+        - The loss parameter is not specified when adding the component, but in the construction of the matrix.
+        
+    - Barrier:
+        - No transformation
+        - Involved all the modes in the interferometer.
+        - Parameters are not used.
+        
+    Note: The barrier component is used for alignment purposes and does not affect the transformation matrix.
+
+    All the matrices are applied to the modes specified by modes. and eventually extended to the full matrix size.
+    
+    Args:
+        modes (tuple): the indices of the modes (the first mode is mode 1)
+        params (float): the parameters of the component, which depend on the type of component
+    """
+
+    def __init__(self,type, modes, params):
+        self.type = type
+        self.modes = modes
+        self.params = params
+
+    def __repr__(self):
+        repr = "\n Component type: {} \n Modes: {} and {} \n Parameters: {:.2f}".format(
+            self.type,
+            self.modes,
+            self.params
+            )
+        return repr
--- circuit/interferometer.py
+++ circuit/interferometer.py
@@ -1,49 +1,19 @@
 import numpy as np
-from .beam_splitter import beam_splitter as bs
+from .component import component as cmp
+from .component import component_type as cmp_type
+from .component import component_color as cmp_color
 
 class interferometer:
     """This class defines an interferometer. 
 
-    An interferometer contains an ordered list of variable beam splitters,
-    represented here by BS_list. For BS in BS_list, BS[0] and BS[1] correspond to the labels of the two modes
-    being interfered (which start at 1). The beam splitters implement the optical transformation defined in equation 1 of:
-    Clements, William R., et al. "Optimal design for universal multiport interferometers." Optica 3.12 (2016): 1460-1465.
-    This transformation is parametrized by BS[2] (theta) which determines the beam splitter reflectivity, 
-    and by BS[3] (phi). The interferometer also contains a list of output phases described by output_phases.
+    An interferometer contains an ordered list of components, represented here by comp_list.
+    For comp in comp_list, comp.modes correspond to the labels of the modes (which start at 1). 
+    The interferometer also contains a list of output phases described by output_phases.
     """
 
     def __init__(self):
-        self.BS_list = []
+        self.comp_list = []
         self.output_phases = []
-
-    def add_BS(self, BS):
-        """Adds a beam splitter at the output of the current interferometer
-
-        Args:
-            BS (Beamsplitter): a Beamsplitter instance
-        """
-        self.BS_list.append(BS)
-    
-    def add_phase_shifter(self,mode,phase):
-        """Use this to manually add a phase shift to a selected mode on a line of the interferometer
-        
-        Args:
-            mode (int): the mode index. The first mode is mode 1
-            phase (float): the real-valued phase to add
-        """
-        # We will add a beam splitter with the same input modes to signal that it's just a phase shifter
-        self.BS_list.append(bs(mode,mode,0,phase))
-
-    def add_output_phase(self, mode, phase):    
-        """Use this to manually add a phase shift to a selected mode at the output of the interferometer
-        
-        Args:
-            mode (int): the mode index. The first mode is mode 1
-            phase (float): the real-valued phase to add
-        """
-        while mode > np.size(self.output_phases):
-            self.output_phases.append(0)
-        self.output_phases[mode-1] = phase
 
     def count_modes(self):          
         """Calculate number of modes involved in the transformation. 
@@ -53,10 +23,10 @@ class interferometer:
         Returns:
             the number of modes in the transformation
         """
-        highest_index = max([max([BS.mode1, BS.mode2]) for BS in self.BS_list])
+        highest_index = max([max(comp.modes) for comp in self.comp_list])
         return highest_index
 
-    def calculate_transformation(self, LN_dB=0.0):       
+    def calculate_transformation(self, LN_dB=0.0):
         """Calculate unitary matrix describing the transformation implemented by the interferometer
     
         Returns:
@@ -65,22 +35,24 @@ class interferometer:
         N = int(self.count_modes())
         U = np.eye(N, dtype=np.complex128)
 
-        for BS in self.BS_list:
+        for comp in self.comp_list:
             T = np.eye(N, dtype=np.complex128)
-            if BS.mode1 != BS.mode2:
-                # Actual beam splitter transformation
-                T[BS.mode1 - 1, BS.mode1 - 1] = np.exp(1j * BS.phi) * np.cos(BS.theta)
-                T[BS.mode1 - 1, BS.mode2 - 1] = -np.sin(BS.theta)
-                T[BS.mode2 - 1, BS.mode1 - 1] = np.exp(1j * BS.phi) * np.sin(BS.theta)
-                T[BS.mode2 - 1, BS.mode2 - 1] = np.cos(BS.theta)
-                # Add loss if specified
-                if LN_dB != 0.0:
-                    loss = 10 ** (-LN_dB / 10)
-                    T[BS.mode1 - 1, BS.mode1 - 1] *= np.sqrt(loss)
-                    T[BS.mode2 - 1, BS.mode2 - 1] *= np.sqrt(loss)
+            if comp.type == cmp_type.BEAM_SPLITTER:
+                T[comp.modes[0] - 1, comp.modes[0] - 1] = np.exp(1j * comp.params[1]) * np.cos(comp.params[0])
+                T[comp.modes[0] - 1, comp.modes[1] - 1] = -np.sin(comp.params[0])
+                T[comp.modes[1] - 1, comp.modes[0] - 1] = np.exp(1j * comp.params[1]) * np.sin(comp.params[0])
+                T[comp.modes[1] - 1, comp.modes[1] - 1] = np.cos(comp.params[0])
+            elif comp.type == cmp_type.PHASE_SHIFTER:
+                T[comp.modes[0] - 1, comp.modes[0] - 1] = np.exp(1j * comp.params[0])
+            elif comp.type == cmp_type.LOSS_ELEMENT:
+                loss = 10 ** (-LN_dB / 10)  # Convert dB to linear scale
+                T[comp.modes[0] - 1, comp.modes[0] - 1] = np.sqrt(loss)
+            elif comp.type == cmp_type.BARRIER:
+                # Barrier does not change the transformation matrix, so we skip it
+                continue
             else:
-                # This is a phase shifter, so we just apply a phase shift to the mode
-                T[BS.mode1-1, BS.mode1-1] = np.exp(1j * BS.phi)
+                raise ValueError("Unknown component type: {}".format(comp.type))
+                
             U = np.matmul(T,U)
 
         while np.size(self.output_phases) < N:  # Autofill for users who don't want to bother with output phases
@@ -104,78 +76,94 @@ class interferometer:
 
         # Draw the input lines
         for ii in range(N):
-            plt.plot((-1, 0), (ii, ii), lw=1, color="black")
+            plt.plot((-1, 0), (ii, ii), lw=1, color=cmp_color.LINE.value)
 
         # Draw the beam splitters / phase shifters
-        for BS in self.BS_list:
-            x = np.max([mode_tracker[BS.mode1 - 1], mode_tracker[BS.mode2 - 1]])
-            if BS.mode1 != BS.mode2:
+        for comp in self.comp_list:
+            if comp.type == cmp_type.BEAM_SPLITTER:
+                color = cmp_color.BEAM_SPLITTER.value
+                x = np.max([mode_tracker[comp.modes[0] - 1], mode_tracker[comp.modes[1] - 1]])
                 # Draw the beam splitter lines
-                plt.plot((x+0.3, x+1), (N - BS.mode1, N - BS.mode2), lw=1, color="lime")
-                plt.plot((x, x+0.3), (N - BS.mode2, N - BS.mode2), lw=1, color="lime")
-                plt.plot((x+0.3, x+1), (N - BS.mode2, N - BS.mode1), lw=1, color="lime")
-                plt.plot((x+0.4, x+0.9), (N - (BS.mode2 + BS.mode1)/2, N - (BS.mode2 + BS.mode1)/2), lw=1, color="lime")
-                reflectivity = "{:2f}".format(np.cos(BS.theta)**2)
-                plt.text(x+0.9, N + 0.05 - (BS.mode2 + BS.mode1)/2, reflectivity[0:3], color="lime", fontsize=7)
-
-            # Draw the phase shifter of the beam splitter (or the stand-alone one)
-            if(BS.phi != 0):
-                plt.plot((x, x+0.3), (N - BS.mode1, N - BS.mode1), lw=1, color="blueviolet")
-                if BS.mode1 != BS.mode2:
-                    plt.plot((x+0.15, x+0.15), (N+0.3-(BS.mode2 + BS.mode1)/2., N+0.7-(BS.mode2 + BS.mode1)/2.), lw=1, color="blueviolet")
-                    circle = plt.Circle((x+0.15, N+0.5-(BS.mode2 + BS.mode1)/2.), 0.1, fill=False, color="blueviolet")
-                else:
-                    plt.plot((x+0.15, x+0.15), (N-0.2-BS.mode1, N+0.2-BS.mode1), lw=1, color="blueviolet")
-                    circle = plt.Circle((x+0.15, N-BS.mode1), 0.1, fill=False, color="blueviolet")
-                plt.gca().add_patch(circle)
-            
-                # Draw the phase label
-                phase = "{:2f}".format(BS.phi)
-                if BS.phi > 0:
-                    if BS.mode1 != BS.mode2:
-                        plt.text(x+0.2, N+0.7-(BS.mode2 + BS.mode1)/2., phase[0:3], color="blueviolet", fontsize=7)
+                plt.plot((x+0.3, x+1), (N - comp.modes[0], N - comp.modes[1]), lw=1, color=color)
+                plt.plot((x, x+0.3), (N - comp.modes[1], N - comp.modes[1]), lw=1, color=color)
+                plt.plot((x+0.3, x+1), (N - comp.modes[1], N - comp.modes[0]), lw=1, color=color)
+                plt.plot((x+0.4, x+0.9), (N - (comp.modes[1] + comp.modes[0])/2, N - (comp.modes[1] + comp.modes[0])/2), lw=1, color=color)
+                reflectivity = "{:2f}".format(np.cos(comp.params[0])**2)
+                plt.text(x+0.9, N + 0.05 - (comp.modes[1] + comp.modes[0])/2, reflectivity[0:3], color=color, fontsize=7)
+                # If the input phase shifter is present (i.e. phi!=0), draw it
+                if comp.params[1] != 0:
+                    plt.plot((x, x+0.3), (N - comp.modes[0], N - comp.modes[0]), lw=1, color=color)
+                    plt.plot((x+0.15, x+0.15), (N+0.3-(comp.modes[1] + comp.modes[0])/2., N+0.7-(comp.modes[1] + comp.modes[0])/2.), lw=1, color=color)
+                    circle = plt.Circle((x+0.15, N+0.5-(comp.modes[1] + comp.modes[0])/2.), 0.1, fill=False, color=color)
+                    plt.gca().add_patch(circle)
+                    # Draw the phase label
+                    phase = "{:2f}".format(comp.params[1])
+                    if comp.params[1] > 0:
+                        plt.text(x+0.2, N+0.7-(comp.modes[1] + comp.modes[0])/2., phase[0:3], color=color, fontsize=7)
                     else:
-                        plt.text(x+0.2, N+0.2-BS.mode1, phase[0:3], color="blueviolet", fontsize=7)
+                        plt.text(x+0.2, N+0.7-(comp.modes[1] + comp.modes[0])/2., phase[0:4], color=color, fontsize=7)
                 else:
-                    if BS.mode1 != BS.mode2:
-                        plt.text(x+0.2, N+0.7-(BS.mode2 + BS.mode1)/2., phase[0:4], color="blueviolet", fontsize=7)
-                    else:
-                        plt.text(x+0.2, N+0.2-BS.mode1, phase[0:4], color="blueviolet", fontsize=7)
-            else:
-                plt.plot((x, x+0.3), (N - BS.mode1, N - BS.mode1), lw=1, color="black")
+                    plt.plot((x, x+0.3), (N - comp.modes[0], N - comp.modes[0]), lw=1, color=cmp_color.LINE.value)
+                # Draw filler lines
+                if x > mode_tracker[comp.modes[0]-1]:
+                    plt.plot((mode_tracker[comp.modes[0]-1], x), (N-comp.modes[0], N-comp.modes[0]), lw=1, color=cmp_color.LINE.value)
+                if x > mode_tracker[comp.modes[1]-1]:
+                    plt.plot((mode_tracker[comp.modes[1]-1], x), (N-comp.modes[1], N-comp.modes[1]), lw=1, color=cmp_color.LINE.value)
+                #Update the mode tracker
+                mode_tracker[comp.modes[0] - 1] = x + 1
+                mode_tracker[comp.modes[1] - 1] = x + 1
                 
-            # Draw filler lines
-            if x > mode_tracker[BS.mode1-1]:
-                plt.plot((mode_tracker[BS.mode1-1], x), (N-BS.mode1, N-BS.mode1), lw=1, color="black")
-            if x > mode_tracker[BS.mode2-1]:
-                plt.plot((mode_tracker[BS.mode2-1], x), (N-BS.mode2, N-BS.mode2), lw=1, color="black")
-
-            # Update the mode tracker
-            if BS.mode1 != BS.mode2:
-                mode_tracker[BS.mode1-1] = x+1
-                mode_tracker[BS.mode2-1] = x+1
+            elif comp.type == cmp_type.PHASE_SHIFTER:
+                color = cmp_color.PHASE_SHIFTER.value
+                x = mode_tracker[comp.modes[0] - 1]
+                # Draw the phase shifter (for understanding purposes, we will draw it even if phi = 0)
+                plt.plot((x, x+0.3), (N - comp.modes[0], N - comp.modes[0]), lw=1, color=color)
+                plt.plot((x+0.15, x+0.15), (N-0.2-comp.modes[0], N+0.2-comp.modes[0]), lw=1, color=color)
+                circle = plt.Circle((x+0.15, N-comp.modes[0]), 0.1, fill=False, color=color)
+                plt.gca().add_patch(circle)
+                # Draw the phase label
+                phase = "{:2f}".format(comp.params[0])
+                if comp.params[0] > 0:
+                    plt.text(x+0.2, N+0.2-comp.modes[0], phase[0:3], color="blueviolet", fontsize=7)
+                else:
+                    plt.text(x+0.2, N+0.2-comp.modes[0], phase[0:4], color="blueviolet", fontsize=7)
+                #Update the mode tracker
+                mode_tracker[comp.modes[0] - 1] = x + 0.3
+                
+            elif comp.type == cmp_type.LOSS_ELEMENT:
+                color = cmp_color.LOSS_ELEMENT.value
+                x = mode_tracker[comp.modes[0] - 1]
+                # Draw the phase shifter (for understanding purposes, we will draw it even if phi = 0)
+                plt.plot((x, x+0.3), (N - comp.modes[0], N - comp.modes[0]), lw=1, color=color)
+                plt.plot((x+0.15, x+0.15), (N-0.2-comp.modes[0], N+0.2-comp.modes[0]), lw=1, color=color)
+                circle = plt.Circle((x+0.15, N-comp.modes[0]), 0.1, fill=False, color=color)
+                plt.gca().add_patch(circle)
+                #Update the mode tracker
+                mode_tracker[comp.modes[0] - 1] = x + 0.3
+            elif comp.type == cmp_type.BARRIER:
+                # Update the mode tracker
+                mode_tracker[comp.modes[0] - 1] = x + 0.3
             else:
-                mode_tracker[BS.mode1-1] = x+0.3
+                raise ValueError("Unknown component type: {}".format(comp.type))
+            
 
         # Draw the output phase shifters
         max_x = np.max(mode_tracker)
+        color = cmp_color.OUTPUT_PHASE.value
         for ii in range(N):
-            plt.plot((mode_tracker[ii], max_x+1), (N-ii-1, N-ii-1), lw=1, color="black")
+            plt.plot((mode_tracker[ii], max_x+1), (N-ii-1, N-ii-1), lw=1, color=cmp_color.LINE.value)
             while np.size(self.output_phases) < N:  # Autofill for users who don't want to bother with output phases
                 self.output_phases.append(0)
             if self.output_phases[ii] != 0:
-                plt.plot((max_x+0.5, max_x+0.5), (N-ii-1.2, N-ii-0.8), lw=1, color="deeppink")
-                circle = plt.Circle((max_x+0.5, N-ii-1), 0.1, fill=False, color="deeppink")
+                plt.plot((max_x+0.5, max_x+0.5), (N-ii-1.2, N-ii-0.8), lw=1, color=color)
+                circle = plt.Circle((max_x+0.5, N-ii-1), 0.1, fill=False, color=color)
                 plt.gca().add_patch(circle)
                 phase = str(self.output_phases[ii])
-                if BS.phi > 0:
-                    plt.text(max_x+0.6, N-ii-0.8, phase[0:3], color="deeppink", fontsize=7)
+                if self.output_phases[ii] > 0:
+                    plt.text(max_x+0.6, N-ii-0.8, phase[0:3], color=color, fontsize=7)
                 else:
-                    plt.text(max_x+0.6, N-ii-0.8, phase[0:4], color="deeppink", fontsize=7)
+                    plt.text(max_x+0.6, N-ii-0.8, phase[0:4], color=color, fontsize=7)
 
-
-        plt.text(max_x/2, -0.7, "lime: BS reflectivity", color="lime", fontsize=10)
-        plt.text(max_x/2, -1.4, "violet: phase shift", color="blueviolet", fontsize=10)
         plt.text(-1, N-0.3, "Light in", fontsize=10)
         plt.text(max_x+0.5, N-0.3, "Light out", fontsize=10)
         plt.gca().axes.set_ylim([-1.8, N+0.2])
